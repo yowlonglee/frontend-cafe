58d4b1a443b71fa6a9e45283da48fcd4898f8532
Yow-Long Lee <yowlonglee@frontendcafe.dev>
2020-04-29 13:23:40 +0800

    Move Commento script source to env files

--- src/components/Post/Commento/Commento.js
+++ src/components/Post/Commento/Commento.js
@@ -25,7 +25,7 @@ const Commento = ({id}) => {
     const document = window.document;
     // In case our #commento container exists we can add our commento script
     if (document.getElementById('commento')) {
-      insertScript(`https://frontend-cafe-commento.herokuapp.com/js/commento.js`, `commento-script`, document.body);
+      insertScript(`${process.env.COMMENTO_SCRIPT_SOURCE}`, `commento-script`, document.body);
     }
 // Cleanup; remove the script from the page
     return () => removeScript(`commento-script`, document.body);
